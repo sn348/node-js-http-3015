79b061f8bd66c05cb6c78e13d537260140b32134
sn348 <sn348@example.com>
2020-05-09 15:17:31 +0900

    すしピザを追加

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req, res) => {
+const pug = require('pug');
+const server = http.createServer((req, res) =>
+{
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
 
-  switch (req.method) {
+  switch (req.method)
+  {
     case 'GET':
-      const fs = require('fs');
-      const rs = fs.createReadStream('./form.html');
-      rs.pipe(res);
+      if (req.url === '/enquetes/yaki-shabu')
+      {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: '焼肉',
+          secondItem: 'しゃぶしゃぶ'
+        }));
+      } else if (req.url === '/enquetes/rice-bread')
+      {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'ごはん',
+          secondItem: 'ぱん'
+        }))
+      }
+      else if (req.url == '/enquetes/sushi-pizza')
+      {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'すし',
+          secondItem: 'ピザ'
+        }))
+      }
+      res.end();
       break;
     case 'POST':
       let rawData = '';
-      req.on('data', (chunk) => {
+      req.on('data', (chunk) =>
+      {
         rawData = rawData + chunk;
-      }).on('end', () => {
+      }).on('end', () =>
+      {
+        const qs = require('querystring');  //name=吉村&yaki-shabu=しゃぶしゃぶのような文字列をパースする
         const decoded = decodeURIComponent(rawData);
         console.info('[' + now + '] 投稿: ' + decoded);
+        const answer = qs.parse(decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+          answer['name'] + 'さんは' + answer['favorite'] +
+          'に投票しました</h1></body></html>');
         res.end();
       });
       break;
     default:
       break;
   }
-}).on('error', (e) => {
+}).on('error', (e) =>
+{
   console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
+}).on('clientError', (e) =>
+{
   console.error('[' + new Date() + '] Client Error', e);
 });
 const port = 8000;
-server.listen(port, () => {
+server.listen(port, () =>
+{
   console.info('[' + new Date() + '] Listening on ' + port);
 });
